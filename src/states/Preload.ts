2cdb4ea2a9bf7f1c29ca9c57c62d88a97268c9b2
Geoffrey Lehée <toxinu@gmail.com>
2017-04-22 18:50:03 +0200

    Renamer master

--- src/states/Preload.ts
+++ src/states/Preload.ts
@@ -7,6 +7,21 @@ export default class Preload extends Phaser.State {
         this.load.image('board', 'assets/images/board.png');
 
         this.load.bitmapFont('carrier-command', 'assets/fonts/carrier_command.png', 'assets/fonts/carrier_command.xml');
+
+        for (var i = 1; i <= 8; i++) {
+            var j = ("00" + i).substr(-2, 2);
+            this.load.audio('keyboard/enter_' + j + '.wav', 'assets/sounds/keyboard/enter_' + j + '.wav')
+        }
+
+        for (var i = 1; i <= 32; i++) {
+            var j = ("00" + i).substr(-2, 2);
+            this.load.audio('keyboard/key_' + j + '.wav', 'assets/sounds/keyboard/key_' + j + '.wav')
+        }
+
+        for (var i = 1; i <= 8; i++) {
+            var j = ("00" + i).substr(-2, 2);
+            this.load.audio('keyboard/space_' + j + '.wav', 'assets/sounds/keyboard/space_' + j + '.wav')
+        }
     }
 
     public create ()
