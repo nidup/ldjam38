2c365bda37a3e52e0ce37296f4c2690266cc8532
Nicolas Dupont <nicolas@akeneo.com>
2017-04-22 22:59:43 +0200

    Add build action

---
+++ src/terminal/actions/build.ts
@@ -0,0 +1,25 @@
+import { Action } from '../action';
+import { Output } from '../output';
+import Play from '../../states/Play';
+import { Biome } from '../../biome/biome';
+
+class Build implements Action {
+    name: string = 'build';
+    module: string;
+
+    constructor(module: string) {
+        this.module = module;
+    }
+
+    execute(state: Play, output: Output) {
+        output.write('I don\'t know how to build...');
+    }
+}
+
+export function BuildActionFactory(parameters: string[]) {
+    if (1 !== parameters.length) {
+        throw 'What do you want to build?!';
+    }
+
+    return new Build(parameters[0]);
+}
--- src/terminal/actions/email.ts
+++ src/terminal/actions/email.ts
@@ -9,7 +9,7 @@ class Email implements Action {
     execute(state: Play, output: Output) {
         output.write('Subject: Get the job done');
         output.write('From: Bernard McLindon');
-        output.write('No suprise, investors are pushing us for exo-planet LD38. You know the drill land the rover on the surface, deploy solar arrays, settle the communication network, install the stock depot and the ore extractor. Be quick, be efficient, don’t lose your time around this rock. And don’t listen to the rover, the new firmware made him… a little poetic. You’ll see.');
+        output.write('No surprise, investors are pushing us for exo-planet LD38. You know the drill land the rover on the surface, deploy solar arrays, settle the communication network, install the stock depot and the ore extractor. Be quick, be efficient, don’t lose your time around this rock. And don’t listen to the rover, the new firmware made him… a little poetic. You’ll see.');
     }
 }
 
