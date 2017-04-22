eeabd222d4b2f6a800df745bd3278ae93e697133
Nicolas Dupont <nicolas@akeneo.com>
2017-04-22 20:10:54 +0200

    Add boot text

--- src/shell.ts
+++ src/shell.ts
@@ -6,29 +6,14 @@ import { GotoActionFactory } from './terminal/actions/goto';
 import { SearchActionFactory } from './terminal/actions/search';
 import { WhereamiActionFactory } from './terminal/actions/whereami';
 import { EmailActionFactory } from './terminal/actions/email';
+import {Output} from "./terminal/output";
 
 export default class Shell {
     private terminal: Terminal;
-
-    private terminalLines = [];
     private shellInput = null;
     private shellText = null;
 
     constructor(state: Play) {
-        let enterKey = state.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-        enterKey.onDown.add(() => {
-            this.shellText.value = this.shellText.value + this.shellInput.value + '\n';
-
-            this.shellText.scrollTop = this.shellText.scrollHeight;
-
-            const output = new ShellOutput(this.shellText);
-            try {
-                this.terminal.getAction(this.shellInput.value).execute(state, output);
-            } catch (e) {
-                output.error(e);
-            }
-            this.shellInput.value = '';
-        }, this);
 
         this.shellInput = document.createElement("input");
         this.shellInput.setAttribute('type', 'text');
@@ -44,11 +29,41 @@ export default class Shell {
         this.shellText.setAttribute('disabled', 'disabled');
         document.body.appendChild(this.shellText);
 
+        const output = new ShellOutput(this.shellText);
+        this.writeBootLines(output);
+
+        let enterKey = state.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        enterKey.onDown.add(() => {
+            this.shellText.value = this.shellText.value + this.shellInput.value + '\n';
+
+            this.shellText.scrollTop = this.shellText.scrollHeight;
+
+            try {
+                this.terminal.getAction(this.shellInput.value).execute(state, output);
+            } catch (e) {
+                output.error(e);
+            }
+            this.shellInput.value = '';
+        }, this);
+
         this.terminal = new Terminal();
         this.terminal.addActionFactory('help', HelpActionFactory);
         this.terminal.addActionFactory('goto', GotoActionFactory);
         this.terminal.addActionFactory('search', SearchActionFactory);
         this.terminal.addActionFactory('whereami', WhereamiActionFactory);
         this.terminal.addActionFactory('email', EmailActionFactory);
+    }
+
+    private writeBootLines(output: Output)
+    {
+        let timeout = 1000;
+        setTimeout(function(){ output.write('Boot...'); }, timeout*1);
+        setTimeout(function(){ output.write('Connect to recon rover...'); }, timeout*2);
+        setTimeout(function(){ output.write('Ping ...'); }, timeout*3);
+        setTimeout(function(){ output.write('Ping ...'); }, timeout*5);
+        setTimeout(function(){ output.write('Ping ...'); }, timeout*7);
+        setTimeout(function(){ output.write('Connection established.'); }, timeout*8);
+        setTimeout(function(){ output.write('Hello operator, i\'m Herb-LB38, your recon rover.'); }, timeout*9);
+        setTimeout(function(){ output.write('Please let me know what to do.'); }, timeout*10);
     }
 }
