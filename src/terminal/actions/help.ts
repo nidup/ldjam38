00b0ba00976dc4c3094d09fa112a6069d299042a
Nicolas Dupont <nicolas@akeneo.com>
2017-04-23 01:43:18 +0200

    re-work whereami action

--- src/terminal/actions/help.ts
+++ src/terminal/actions/help.ts
@@ -12,11 +12,11 @@ class Help implements Action {
     execute(state: Play, output: Output) {
         output.writeToTerminal(                       'help               -- display helps');
         let timeout = 200;
-        setTimeout(function(){ output.writeToTerminal('goto [destination] -- move to destination'); }, timeout*1);
-        setTimeout(function(){ output.writeToTerminal('build [module]     -- build the module'); }, timeout*2);
+        setTimeout(function(){ output.writeToTerminal('goto [location]    -- move to location'); }, timeout*1);
+        setTimeout(function(){ output.writeToTerminal('build [module]     -- build a module'); }, timeout*2);
         setTimeout(function(){ output.writeToTerminal('email              -- open your inbox'); }, timeout*3);
-        setTimeout(function(){ output.writeToTerminal('whereami           -- describe current location'); }, timeout*4);
-        setTimeout(function(){ output.writeToTerminal('search             -- advanced analysis of location'); }, timeout*5);
+        setTimeout(function(){ output.writeToTerminal('whereami           -- locations'); }, timeout*4);
+        setTimeout(function(){ output.writeToTerminal('search             -- analysis of location'); }, timeout*5);
     }
 }
 
