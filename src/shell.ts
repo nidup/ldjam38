93005c3f0fa0364365fe0d1a3c4291c6fc66235e
Gildas Quéméner <gildas.quemener@gmail.com>
2017-04-23 14:57:29 +0200

    Added final hdd sounds

--- src/shell.ts
+++ src/shell.ts
@@ -59,6 +59,7 @@ export default class Shell {
 
         this.writeBootLines(this.output, () => {
             let enterKey = this.state.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+            let lastHddLoadSoundPlayed = 1;
             enterKey.onDown.add(() => {
                 if (this.shellInput.value == "") {
                     return;
@@ -66,7 +67,11 @@ export default class Shell {
                 this.shellText.value = this.shellText.value + this.shellInput.value + '\n';
                 this.shellInput.setAttribute('disabled', true);
 
-                const stopSound = this.output.playToSpeaker('hdd/load');
+                const stopSound = this.output.playToSpeaker('hdd/load/'+lastHddLoadSoundPlayed++);
+                if (lastHddLoadSoundPlayed > 8) {
+                    lastHddLoadSoundPlayed = 1;
+                }
+
                 try {
                     this.terminal
                         .getAction(this.shellInput.value)
