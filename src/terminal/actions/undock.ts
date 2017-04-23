0ccaf2176a4e5584c4f8ab5da33c3f656ce27522
Gildas Quéméner <gildas.quemener@gmail.com>
2017-04-23 19:05:04 +0200

    Fixed syntac in undock action

--- src/terminal/actions/undock.ts
+++ src/terminal/actions/undock.ts
@@ -14,34 +14,35 @@ class Undock implements Action {
                 return;
             }
 
-        output.writeToTerminal('Wow, finally!', false, true);output.writeToTerminal(':: Undocking procedure started');
-        let spaceshipSound = new SpaceshipSound(state);
-        spaceshipSound.playUndock();
+            output.writeToTerminal('Wow, finally!', false, true);output.writeToTerminal(':: Undocking procedure started');
+            let spaceshipSound = new SpaceshipSound(state);
+            spaceshipSound.playUndock();
 
-            setTimeout(() => { output.writeToTerminal(':: Depressuring process...'); }, 2000);
-            setTimeout(() => { output.writeToTerminal(':: Undocking rover... please stay vigilant.'); }, 15000);
-            setTimeout(() => {
-                output.writeToTerminal('Undocking process succeeded!');
-                state.isRoverLanded = true;
-                // always land on snowy-forest, you can't do nothing here
-                state.currentLocation = state.locations.find((location) => location.name === 'ice-field');
+                setTimeout(() => { output.writeToTerminal(':: Depressuring process...'); }, 2000);
+                setTimeout(() => { output.writeToTerminal(':: Undocking rover... please stay vigilant.'); }, 15000);
+                setTimeout(() => {
+                    output.writeToTerminal('Undocking process succeeded!');
+                    state.isRoverLanded = true;
+                    // always land on snowy-forest, you can't do nothing here
+                    state.currentLocation = state.locations.find((location) => location.name === 'ice-field');
 
-            }, 28000);
+                }, 28000);
+                setTimeout(() => {
+                    output.writeToTerminal('Boot...');
+                    output.writeToTerminal('Connect to recon rover...');
+                }, 28500);
+            setTimeout(() => { output.writeToTerminal('Ping...'); }, 29000);
+            setTimeout(() => { output.writeToTerminal('Ping...'); }, 30000);
+            setTimeout(() => { output.writeToTerminal('Ping...'); }, 31000);
+            setTimeout(() => { output.writeToTerminal('Connection established.'); }, 32000);
             setTimeout(() => {
-                output.writeToTerminal('Boot...');
-                output.writeToTerminal('Connect to recon rover...');
-            }, 28500);
-        setTimeout(() => { output.writeToTerminal('Ping...'); }, 29000);
-        setTimeout(() => { output.writeToTerminal('Ping...'); }, 30000);
-        setTimeout(() => { output.writeToTerminal('Ping...'); }, 31000);
-        setTimeout(() => { output.writeToTerminal('Connection established.'); }, 32000);
-        setTimeout(() => {
-            output.writeToTerminal(
-                'Hello operator, I\'m VJ-Net38, your recon rover. I\'ve finally ' +
-                'landed on this small planet, feel free to send instructions.', false, true);
-            output.writeToTerminal(state.currentLocation.description, false, true);
-            resolve();
-        }, 33000);
+                output.writeToTerminal(
+                    'Hello operator, I\'m VJ-Net38, your recon rover. I\'ve finally ' +
+                    'landed on this small planet, feel free to send instructions.', false, true);
+                output.writeToTerminal(state.currentLocation.description, false, true);
+                resolve();
+            }, 33000);
+        });
     }
 }
 
