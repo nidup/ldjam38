d170c94397a628566c1172040b09ca441382793d
Geoffrey Lehée <toxinu@gmail.com>
2017-04-23 23:32:32 +0200

    Update help

--- src/terminal/actions/help.ts
+++ src/terminal/actions/help.ts
@@ -11,16 +11,17 @@ class Help implements Action {
 
     execute(state: Play, output: Output) {
         return new Promise(function(resolve) {
-            output.writeToTerminal(                       'help               -- display helps');
+            output.writeToTerminal(                       'help                -- display help');
             let timeout = 200;
-            setTimeout(function(){ output.writeToTerminal('build [module]     -- build a module'); }, timeout*1);
-            setTimeout(function(){ output.writeToTerminal('goto [location]    -- move to location'); }, timeout*2);
-            setTimeout(function(){ output.writeToTerminal('inbox              -- check your inbox'); }, timeout*3);
-            setTimeout(function(){ output.writeToTerminal('inventory          -- check rover inventory'); }, timeout*4);
-            setTimeout(function(){ output.writeToTerminal('undock             -- undock rover from pod'); }, timeout*5);
-            setTimeout(function(){ output.writeToTerminal('whereami           -- current/known locations'); }, timeout*6);
-            setTimeout(function(){ output.writeToTerminal('archive ls, show   -- browse archives'); }, timeout*7);
-            setTimeout(function(){ output.writeToTerminal('explore            -- explore current location'); resolve(); }, timeout*8);
+            setTimeout(function(){ output.writeToTerminal('build [module]      -- build a module'); }, timeout*1);
+            setTimeout(function(){ output.writeToTerminal('goto [location]     -- move to location'); }, timeout*2);
+            setTimeout(function(){ output.writeToTerminal('inbox               -- check your inbox'); }, timeout*3);
+            setTimeout(function(){ output.writeToTerminal('inventory           -- list rover modules'); }, timeout*4);
+            setTimeout(function(){ output.writeToTerminal('undock              -- undock rover from pod'); }, timeout*5);
+            setTimeout(function(){ output.writeToTerminal('whereami            -- current/known locations'); }, timeout*6);
+            setTimeout(function(){ output.writeToTerminal('archive ls          -- list archives'); }, timeout*7);
+            setTimeout(function(){ output.writeToTerminal('archive open [name] -- open an archive'); }, timeout*7);
+            setTimeout(function(){ output.writeToTerminal('explore             -- explore current location'); resolve(); }, timeout*8);
         });
     }
 }
