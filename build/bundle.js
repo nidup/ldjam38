8bfdacf1d307a7074329b36490adc47ca6e56576
Geoffrey Lehée <toxinu@gmail.com>
2017-04-23 23:34:50 +0200

    Update bundle

--- build/bundle.js
+++ build/bundle.js
@@ -1111,6 +1111,7 @@ class Preload extends Phaser.State {
         this.load.image('cinematic2', 'assets/images/cinematic2.png');
         this.load.image('board', 'assets/images/board.png');
         this.load.image('board_top', 'assets/images/board_top.png');
+        this.load.image('board_top_dark', 'assets/images/board_top_dark.png');
         this.load.image('scenes/planet1', 'assets/images/scenes/planet1.png');
         this.load.image('scenes/planet2', 'assets/images/scenes/planet2.png');
         this.load.audio('music/intro', 'assets/sounds/musique_intro.mp3');
@@ -1767,16 +1768,17 @@ class Help {
     }
     execute(state, output) {
         return new Promise(function (resolve) {
-            output.writeToTerminal('help               -- display helps');
+            output.writeToTerminal('help                -- display help');
             let timeout = 200;
-            setTimeout(function () { output.writeToTerminal('build [module]     -- build a module'); }, timeout * 1);
-            setTimeout(function () { output.writeToTerminal('goto [location]    -- move to location'); }, timeout * 2);
-            setTimeout(function () { output.writeToTerminal('inbox              -- check your inbox'); }, timeout * 3);
-            setTimeout(function () { output.writeToTerminal('inventory          -- check rover inventory'); }, timeout * 4);
-            setTimeout(function () { output.writeToTerminal('undock             -- undock rover from pod'); }, timeout * 5);
-            setTimeout(function () { output.writeToTerminal('whereami           -- current/known locations'); }, timeout * 6);
-            setTimeout(function () { output.writeToTerminal('archive ls, show   -- browse archives'); }, timeout * 7);
-            setTimeout(function () { output.writeToTerminal('explore            -- explore current location'); resolve(); }, timeout * 8);
+            setTimeout(function () { output.writeToTerminal('build [module]      -- build a module'); }, timeout * 1);
+            setTimeout(function () { output.writeToTerminal('goto [location]     -- move to location'); }, timeout * 2);
+            setTimeout(function () { output.writeToTerminal('inbox               -- check your inbox'); }, timeout * 3);
+            setTimeout(function () { output.writeToTerminal('inventory           -- list rover modules'); }, timeout * 4);
+            setTimeout(function () { output.writeToTerminal('undock              -- undock rover from pod'); }, timeout * 5);
+            setTimeout(function () { output.writeToTerminal('whereami            -- current/known locations'); }, timeout * 6);
+            setTimeout(function () { output.writeToTerminal('archive ls          -- list archives'); }, timeout * 7);
+            setTimeout(function () { output.writeToTerminal('archive open [name] -- open an archive'); }, timeout * 7);
+            setTimeout(function () { output.writeToTerminal('explore             -- explore current location'); resolve(); }, timeout * 8);
         });
     }
 }
@@ -1881,7 +1883,10 @@ class Undock {
             let spaceshipSound = new spaceship_1.default(state);
             spaceshipSound.playUndock();
             setTimeout(() => { output.writeToTerminal(':: Depressuring process...'); }, 2000);
-            setTimeout(() => { output.writeToTerminal(':: Undocking rover... please stay vigilant.'); }, 15000);
+            setTimeout(() => {
+                this.shake(state);
+                output.writeToTerminal(':: Undocking rover... please stay vigilant.');
+            }, 15000);
             setTimeout(() => {
                 output.writeToTerminal('Undocking process succeeded!');
                 state.isRoverLanded = true;
@@ -1903,6 +1908,36 @@ class Undock {
                 resolve();
             }, 33000);
         });
+    }
+    shake(state) {
+        var canvas = document.getElementsByTagName('canvas')[0];
+        var shellText = document.getElementById('shellText');
+        canvas.classList.add('shake');
+        shellText.classList.add('shake');
+        let darkBoard = state.foreground.create(0, 0, 'board_top_dark');
+        darkBoard.width = darkBoard.width * 1.5;
+        darkBoard.height = darkBoard.height * 1.5;
+        setTimeout(() => {
+            darkBoard.kill();
+        }, 200);
+        setTimeout(() => {
+            canvas.classList.remove('shake');
+            shellText.classList.remove('shake');
+        }, 2000);
+        setTimeout(() => {
+            canvas.classList.add('shake');
+            shellText.classList.add('shake');
+            let darkBoard = state.foreground.create(0, 0, 'board_top_dark');
+            darkBoard.width = darkBoard.width * 1.5;
+            darkBoard.height = darkBoard.height * 1.5;
+            setTimeout(() => {
+                darkBoard.kill();
+            }, 200);
+        }, 3000);
+        setTimeout(() => {
+            canvas.classList.remove('shake');
+            shellText.classList.remove('shake');
+        }, 4000);
     }
 }
 function UndockActionFactory(parameters) {
